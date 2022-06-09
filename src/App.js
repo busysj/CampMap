70c78712b90be3cad81337055cdcaa7d3f52f549
chaeeon <99634900+bins0@users.noreply.github.com>
2022-06-09 16:33:41 +0900

    채언 수정

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Community from "./components/CommunityPage/Community";
 import MapPage from './page/MapPage';
 
 function App() {
-    return (
-        <div className="App">
-            <NavbarPage />
-            <Routes>
-                <Route path="/" element={<MainPage />}></Route>
-                <Route path="/map" element={<MapPage />}></Route>
-                <Route path="/community" element={<Community />}></Route>
-            </Routes>
-            <FooterPage />
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavbarPage />
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/map" element={<MapPage />}></Route>
+        <Route path="/community" element={<Community />}></Route>
+      </Routes>
+      <FooterPage />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MainPage/PopupPage.jsx
+++ src/components/MainPage/PopupPage.jsx
@@ -6,71 +6,71 @@ import Safety from "../../assets/Safety.png";
 // import { Modal } from "antd";
 
 const Popup = styled.div`
-    position: fixed;
-    width: 350px;
-    height: 500px;
-    border-radius: 5px;
-    box-shadow: 0px 0px 3px 3px gray;
-    left: 20%;
-    top: 20%;
-    transform: translate(-20%, -20%);
-    z-index: 5;
+  position: fixed;
+  width: 350px;
+  height: 500px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 3px 3px gray;
+  left: 20%;
+  top: 20%;
+  transform: translate(-20%, -20%);
+  z-index: 5;
 `;
 const PopupImage = styled.img`
-    border-radius: 5px;
-    width: 350px;
-    height: 500px;
+  border-radius: 5px;
+  width: 350px;
+  height: 500px;
 `;
 
 const PopupHeader = styled.div`
-    position: absolute;
-    width: 350px;
-    height: 40px;
-    background: black;
-    opacity: 0.4;
-    border-radius: 5px 5px 0 0;
-    &:hover {
-        opacity: 0.7;
-    }
+  position: absolute;
+  width: 350px;
+  height: 40px;
+  background: black;
+  opacity: 0.4;
+  border-radius: 5px 5px 0 0;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 const PopupButton = styled.button`
-    /* position: absolute; */
-    /* width: 40px;
+  /* position: absolute; */
+  /* width: 40px;
   height: 40px;
   border-radius: 50%; */
-    background: black;
-    border: none;
-    opacity: 0.65;
-    color: #fff;
-    font-size: 1.5rem;
-    margin: 6px 12px 0 0;
-    cursor: pointer;
-    float: right;
+  background: black;
+  border: none;
+  opacity: 0.65;
+  color: #fff;
+  font-size: 1.5rem;
+  margin: 0 12px 0 0;
+  cursor: pointer;
+  float: right;
 
-    &:hover {
-        opacity: 1;
-    }
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const PopupPage = (props) => {
-    const { onClose } = props;
-    return (
-        <Draggable>
-            <Popup>
-                <PopupHeader>
-                    <PopupButton
-                        onClick={() => {
-                            onClose(false);
-                        }}
-                    >
-                        X
-                    </PopupButton>
-                </PopupHeader>
-                <PopupImage src={Safety} to="/" />
-            </Popup>
-        </Draggable>
-    );
+  const { onClose } = props;
+  return (
+    <Draggable>
+      <Popup>
+        <PopupHeader>
+          <PopupButton
+            onClick={() => {
+              onClose(false);
+            }}
+          >
+            X
+          </PopupButton>
+        </PopupHeader>
+        <PopupImage src={Safety} to="/" />
+      </Popup>
+    </Draggable>
+  );
 };
 
 export default PopupPage;
