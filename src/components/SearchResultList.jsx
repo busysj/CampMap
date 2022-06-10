623ddc36501524810877fdbbb77a111aa4464893
SJ <tkdwox31__@naver.com>
2022-06-10 16:49:04 +0900

    0610 list scroll

--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -3,8 +3,15 @@ import { List } from "antd";
 import styled from "styled-components";
 import Campsite from "../assets/Camping06.jpg";
 
+const SearchBox = styled.div`
+  height: 700px;
+  padding: 15px;
+  overflow-y: scroll;
+`;
+
 const SearchList = styled(List)`
   margin-top: 10px;
+  /* text-align: center; */
 `;
 
 const data = Array.from({
@@ -18,29 +25,32 @@ const data = Array.from({
 
 const SearchResultList = () => {
   return (
-    <SearchList
-      itemLayout="vertical"
-      size="large"
-      pagination={{
-        onChange: (page) => {
-          console.log(page);
-        },
-        pageSize: 4,
-      }}
-      dataSource={data}
-      renderItem={(item) => (
-        <List.Item
-          key={item.title}
-          extra={<img width={200} alt="캠핑장 사진" src={Campsite} />}
-        >
-          <List.Item.Meta
-            title={<a href={item.href}>{item.title}</a>}
-            description={item.address}
-          />
-          {item.callnumber}
-        </List.Item>
-      )}
-    />
+    <SearchBox>
+      <SearchList
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 5,
+        }}
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            extra={<img width={200} alt="캠핑장 사진" src={Campsite} />}
+          >
+            <List.Item.Meta
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.address}
+            />
+            {item.callnumber} <br />
+            <button>예약</button>
+          </List.Item>
+        )}
+      />
+    </SearchBox>
   );
 };
 
