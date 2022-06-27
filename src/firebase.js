71ef0e25edc329e40972dcf3392edf89ddae9ac6
Jaerazone <seren0120@naver.com>
2022-06-27 23:03:30 +0900

    ✅feat : firestore db 가져오기, DetailPage 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,24 @@
 import { initializeApp } from "firebase/app";
-import {
-    getAuth, // 인증
-} from "firebase/auth";
+import { getAuth } from "firebase/auth"; // 인증
 import { getFirestore } from "firebase/firestore"; // firestore 사용
+import { getStorage } from "firebase/firestore";
 
 const firebaseConfig = {
     // firebase 설정과 관련된 개인 정보
-    apiKey: "AIzaSyC_BGyPvbcV2JTw5HfAS_yDdUWUlg8RLUo",
-    authDomain: "campproject-ee34a.firebaseapp.com",
-    projectId: "campproject-ee34a",
-    storageBucket: "campproject-ee34a.appspot.com",
-    messagingSenderId: "1048265201053",
-    appId: "1:1048265201053:web:2eebe695834072367b8b74"
+    apiKey: "AIzaSyC0DCwci9mhJSYr2nHHlzxySmrReidnCgQ",
+    authDomain: "campmap-983f8.firebaseapp.com",
+    projectId: "campmap-983f8",
+    storageBucket: "campmap-983f8.appspot.com",
+    messagingSenderId: "218811450222",
+    appId: "1:218811450222:web:1749df7ad4d1ef6b7bb77f",
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-// 이런식으로 쓰면 사용할 js파일에 import {db} from '~~firebase.js' 하면됨
-
 export const auth = getAuth();
 // import { signInWithEmailAndPassword } from "firebase/auth";
 // import { auth } from "../../firebase";
 // 사용할 js파일에서 이렇게 사용하믄댐
+
+export const db = getFirestore(app);
+// 이런식으로 쓰면 사용할 js파일에 import db from '~~firebase.js' 하면됨
+//export const storage = getStorage(app);
