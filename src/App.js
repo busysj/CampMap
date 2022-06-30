8552e47711f9c2c90d3550c49501001a4bea3e7a
SJ <tkdwox31__@naver.com>
2022-06-30 17:51:06 +0900

    0630 ysj

--- src/App.js
+++ src/App.js
@@ -19,46 +19,48 @@ import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
 import { selectUser } from "./store/userSlice";
 
-
 function App() {
-    const user = useSelector(selectUser);
-    console.log(`gggg userㄱㅏ ㅇㅓㅂㅅㅇㅏ?`, user);
+  const user = useSelector(selectUser);
+  console.log(`gggg userㄱㅏ ㅇㅓㅂㅅㅇㅏ?`, user);
 
-    return (
-        <div className="App">
-            <ToastContainer />
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/camppage" element={<CampPage />} />
-                    <Route path="/map" element={<MapPage />}>
-                        <Route index element={<SearchResultList />} />
-                    </Route>
-                    <Route path="/camppage" element={<CampPage />} />
-                    <Route path="/community" element={<Community user={user} />} />
-                    <Route path="/detail/:id" element={<DetailPage />} />
-                    /
-                    <Route
-                        path="/create"
-                        element={user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />}
-                    />
-                    {/* 😥user?.uid => user && user.uid */}
-                    <Route
-                        path="/update/:id"
-                        element={user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />}
-                    />
-                    {/* 로그인창 팝업이라 그런지 실행이 안됨..
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/map" element={<MapPage />}>
+            <Route index element={<SearchResultList />} />
+          </Route>
+          <Route path="/camppage/:id" element={<CampPage />} />
+          <Route path="/community" element={<Community user={user} />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          /
+          <Route
+            path="/create"
+            element={
+              user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />
+            }
+          />
+          {/* 😥user?.uid => user && user.uid */}
+          <Route
+            path="/update/:id"
+            element={
+              user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />
+            }
+          />
+          {/* 로그인창 팝업이라 그런지 실행이 안됨..
                     {!user ? (
                         <Route path="/" element={<LogReg />}></Route>
                     ) : (
                         <Route path="/create" element={<AddEditBlog user={user} />}></Route>
                     )}*/}
-                    <Route path="/youtube" element={<CampingNav />} />
-                    <Route path="*" element={<NotFound />}></Route>
-                </Route>
-            </Routes>
-        </div>
-    );
+          <Route path="/youtube" element={<CampingNav />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
