4e7ac1ca3cc6bc937acba6c8b5b8019147d46154
unknown <princewns@gmail.com>
2022-07-19 15:10:09 +0900

    '220719_로케이션_수정중'

--- src/components/mapApi/LocationBaseList.jsx
+++ src/components/mapApi/LocationBaseList.jsx
@@ -12,31 +12,17 @@ const geolocationOptions = {
 const LocationBaseList = () => {
   const dispatch = useDispatch();
   const {location, error} = UseCurrentLocation(geolocationOptions);
+
   useEffect(() => {
-    if(location){
       console.log(location);
       axios.get(
         `http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/locationBasedList?ServiceKey=DBx1v7ble2j4MNFWznYeeM5wQYthH5QTVeMOTXn5H%2FxvLP7Bbaa8IZvKxHq8r0425fyEMXvrs32EFDRIALvz5A%3D%3D&mapX=${location.longitude}&mapY=${location.latitude}&radius=20000&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json`
       ).then((response) => {
-          return dispatch(addLocationData(response.data.response.body.items.item));
-        //dispatch(loadingState(false));
+          dispatch(addLocationData(response.data.response.body.items.item));
+          dispatch(loadingState(false));
       })
     .catch((error) => {
       console.log(error);
-      //dispatch(loadingState(true));
-    });
-    }
-    else{
-      axios.get(
-        `http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/locationBasedList?ServiceKey=DBx1v7ble2j4MNFWznYeeM5wQYthH5QTVeMOTXn5H%2FxvLP7Bbaa8IZvKxHq8r0425fyEMXvrs32EFDRIALvz5A%3D%3D&mapX=126.976811&mapY=37.575854&radius=20000&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json`
-      ).then((response) => {
-          return dispatch(addLocationData(response.data.response.body.items.item));
-        //dispatch(loadingState(false));
-      })
-    .catch((error) => {
-      console.log(error);
-      //dispatch(loadingState(true));
-    });
-    }},[dispatch,location]);
+    })},[dispatch, location]);
 };
 export default LocationBaseList;
--- src/hooks/UseCurrentLocation.jsx
+++ src/hooks/UseCurrentLocation.jsx
@@ -2,10 +2,7 @@ import { useState, useEffect } from "react";
 
 const UseCurrentLocation = (options = {}) => {
   // location 정보 저장
-  const [location, setLocation] = useState({
-    latitude : null,
-    longitude : null
-  });
+  const [location, setLocation] = useState();
   // 에러 메세지 저장
   const [error, setError] = useState();
 
@@ -14,15 +11,14 @@ const UseCurrentLocation = (options = {}) => {
     const { latitude, longitude } = pos.coords;
 
     setLocation({
-      latitude : latitude,
-      longitude : longitude,
+      latitude,
+      longitude
     });
   };
 
   // Geolocation의 `getCurrentPosition` 메소드에 대한 실패 callback 핸들러
   const handleError = (error) => {
     setError(error.message);
-
   };
 
   useEffect(() => {
@@ -31,6 +27,10 @@ const UseCurrentLocation = (options = {}) => {
     // 사용된 브라우저에서 지리적 위치(Geolocation)가 정의되지 않은 경우 오류로 처리합니다.
     if (!geolocation) {
       setError("Geolocation is not supported.");
+      setLocation({
+        latitude : 37.575984,
+        longitude : 126.976828
+      });
       return;
     }
 
